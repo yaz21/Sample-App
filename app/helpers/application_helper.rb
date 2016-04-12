99cbddabff456ea3bdc774469b6b2b2f232ee196
yaz <contact@pan2view.com>
2016-04-12 22:36:11 +0100

    lesson 4

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,16 @@
 module ApplicationHelper
+
+#return the full title on a per-page basis.
+
+def full_title(page_title = "")
+  base_title =  "Ruby on Rails Tutorial App"
+
+  if page_title.empty?
+    base_title
+  else
+    "#{page_title} | #{base_title}"
+  end
+
+  end
+
 end
